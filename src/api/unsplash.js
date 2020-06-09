d0068d90a6c1bd6a648e904b6f7698429d770e48
Sidharth Mohanty <47181289+sidharthmohanty@users.noreply.github.com>
2020-06-09 09:39:22 +0530

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID #access-key",
+    Authorization: "Client-ID kFOkWVEqWBn2L-T_Z6mdPJpYRVXlJaevgwLv1oy8cWU",
   },
 });
